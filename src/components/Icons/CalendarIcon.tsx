f8e987de74801b02382aff91263d137c3061276d
Lucas Neuhaus <lucas.neuhaus93@gmail.com>
2023-03-28 11:55:52 -0300

    Completed create modal

---
+++ src/components/Icons/CalendarIcon.tsx
@@ -0,0 +1,11 @@
+import { Circle, Path, Svg } from 'react-native-svg';
+import type { IconProps } from './IconProps';
+
+const CalendarIcon: React.FC<IconProps> = (props) => (
+    <Svg width="24" height="24" viewBox="0 0 24 24" {...props}>
+        <Path d="M18 4h-1V3a1 1 0 0 0-2 0v1H9V3a1 1 0 0 0-2 0v1H6a3 3 0 0 0-3 3v12a3 3 0 0 0 3 3h12a3 3 0 0 0 3-3V7a3 3 0 0 0-3-3zM6 6h1v1a1 1 0 0 0 2 0V6h6v1a1 1 0 0 0 2 0V6h1a1 1 0 0 1 1 1v4H5V7a1 1 0 0 1 1-1zm12 14H6a1 1 0 0 1-1-1v-6h14v6a1 1 0 0 1-1 1z" />
+        <Circle cx="8" cy="16" r="1" />
+    </Svg>
+);
+
+export default CalendarIcon;
---
+++ src/components/Icons/index.ts
@@ -0,0 +1,4 @@
+import CalendarIcon from './CalendarIcon';
+import SendIcon from './SendIcon';
+
+export { CalendarIcon, SendIcon };
