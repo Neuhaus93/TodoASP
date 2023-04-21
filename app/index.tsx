4f1bbcd1f3cff604164914280c8fb5899821b71a
Lucas Neuhaus <38574361+Neuhaus93@users.noreply.github.com>
2023-04-21 16:29:07 -0300

    Add splash screen (#31)

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,21 @@
-import { Redirect } from 'expo-router';
+import { Redirect, SplashScreen } from 'expo-router';
+import { useEffect, useState } from 'react';
+import { useTasks } from '../src/features/Tasks/queries';
 
 export default function HomePage() {
+    const { isSuccess, isError } = useTasks();
+    const [ready, setReady] = useState(false);
+
+    // When the tasks query settles, remove the splash screen
+    useEffect(() => {
+        if (!ready && (isSuccess || isError)) {
+            setReady(true);
+        }
+    }, [ready, isSuccess, isError]);
+
+    if (!ready) {
+        return <SplashScreen />;
+    }
+
     return <Redirect href="/tasks/inbox" />;
 }
